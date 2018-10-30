6642bf4d78a4bd46fd7173eef0c49feb49a4e889
Jason Campbell <jlcampbell86@gmail.com>
2018-10-30 13:10:50 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(args)
+    args.each do |k, v|
+      instance_variable_set("@#{k}", v)
+    end
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
