9b75418d3f6ff5bca1a7154216397c06d3f315c8
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-10-30 15:36:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,30 @@
 require './environment'
-
+require 'pry'
 module FormsLab
+
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    # set :views, "app/views/pirates"
+
+    get '/' do
+      erb :opening
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      @ships= params[:pirate][:ships].collect do |details|
+        Ship.new(details)
+      end
+
+      erb :show
+
+    end
+
 
   end
 end
